f0abbbd55299f44ca3d8bd1c7d863678a8516875
Roham Zendehdel <rzninvo@gmail.com>
2023-06-22 20:17:26 +0330

    Project Phase 4 update

--- src/controller.py
+++ src/controller.py
@@ -3,6 +3,8 @@
 # ROS
 import rospy
 from geometry_msgs.msg import Twist
+from turtlebot3_object_tracker.srv import DetectionData, DetectionDataResponse
+import math
 
 
 class Controller:
@@ -12,17 +14,56 @@ class Controller:
         self.move.linear.x = 0.1
         self.freeze = Twist()
 
+
+        # Bounding-box data
+        self.bb_cx = 0
+        self.bb_cy = 0
+        self.bb_width = 0
+        self.bb_height = 0
+        self.img_width = 0
+        self.img_height = 0
+        
         # The "p" parameter for your p-controller, TODO: you need to tune this
-        self.angular_vel_coef = 1
+        self.angular_vel_coef = 0.005
 
         # TODO: Create a service proxy for your human detection service
+        self.get_bb_from_server('person')
+
         
         # TODO: Create a publisher for your robot "cmd_vel"
-    
+        self.cmd_vel = rospy.Publisher('/follower/cmd_vel', Twist, queue_size=5)
+
+    def get_bb_from_server(self, label):
+        rospy.wait_for_service('human_detection_server')
+        try:
+            gbb_data_service = rospy.ServiceProxy('human_detection_server', DetectionData)
+            response : DetectionDataResponse = gbb_data_service(label)
+            self.bb_cx = response.bb_cx
+            self.bb_cy = response.bb_cy
+            self.bb_width = response.bb_width
+            self.bb_height = response.bb_height
+            self.img_width = response.img_width
+            self.img_height = response.img_height
+            self.flag = response.flag
+            
+        except rospy.ServiceException as e:
+           rospy.loginfo(f'ERROR from human_detection_server service: {e}')
+
+    def calculate_rotation_angle(self):
+        img_center_x = self.img_width / 2
+        img_center_y = self.img_height / 2
+        return img_center_x - self.bb_cx
+
     def run(self) -> None:
         try:
             while not rospy.is_shutdown():
                 # TODO: Call your service, ride your robot
+                self.get_bb_from_server('person')
+                err = self.calculate_rotation_angle()
+                angular_P = self.angular_vel_coef * err
+                if self.flag == 'found':
+                    self.move.angular.z = angular_P
+                    self.cmd_vel.publish(self.move)
                 pass
 
         except rospy.exceptions.ROSInterruptException:
--- src/image_processor.py
+++ src/image_processor.py
@@ -9,6 +9,7 @@ import numpy as np
 from ultralytics import YOLO
 from ultralytics.yolo.utils.plotting import Annotator
 from ultralytics.yolo.engine.results import Results
+from turtlebot3_object_tracker.srv import DetectionData, DetectionDataResponse
 
 # ROS
 import rospy
@@ -23,21 +24,22 @@ class ImageProcessor:
         self.image_res = 240, 320, 3 # Camera resolution: height, width
         self.image_np = np.zeros(self.image_res) # The numpy array to pour the image data into
 
+        # TODO: Instantiate your YOLO object detector/classifier model
+        self.model: YOLO = YOLO('/home/rohamzn/Robotics-Course/catkin_ws/src/turtlebot3_object_tracker/yolo/yolov8n.pt')
+
         # TODO: Subscribe on your robot's camera topic
         # NOTE: Make sure you use the provided listener for this subscription
-        self.camera_subscriber = None
+        self.camera_subscriber = rospy.Subscriber('/follower/camera/image', Image, callback=self.camera_listener)
 
-        # TODO: Instantiate your YOLO object detector/classifier model
-        self.model: YOLO = None
         # TODO: You need to update results each time you call your model
-        self.results: Results = None
+        self.results: Results = self.model(self.image_np)
 
         self.cv2_frame_size = 400, 320
         cv2.namedWindow("robot_view", cv2.WINDOW_NORMAL)
         cv2.resizeWindow("robot_view", *self.cv2_frame_size)
 
         # TODO: Setup your "human detection" service
-        self.human_detection_server = None
+        self.human_detection_server = rospy.Service('human_detection_server', DetectionData, self.get_detection_data)
 
         self.update_view()
 
@@ -52,20 +54,61 @@ class ImageProcessor:
                 if len(self.image_msg.data) == 0: # If there is no image data
                     continue
 
+
                 # Convert binary image data to numpy array
                 self.image_np = np.frombuffer(self.image_msg.data, dtype=np.uint8)
                 self.image_np = self.image_np.reshape(self.image_res)
 
-                frame = copy.deepcopy(self.image_np)
-
+                # Predicting results
+                self.results = self.model(self.image_np, verbose = False)
+                res_plotted = self.results[0].plot()
+                #frame = copy.deepcopy(self.image_np)
                 # TODO: You can use an "Annotator" to draw object bounding boxes on frame
+                # self.annotator : Annotator = Annotator(self.image_np)
+                # self.annotator.box_label(self.results[0].boxes[0], label= 'Person')
+                # result_image = self.annotator.result()
 
-                cv2.imshow("robot_view", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+                cv2.imshow("robot_view", cv2.cvtColor(res_plotted, cv2.COLOR_RGB2BGR))
                 cv2.waitKey(1)
 
         except rospy.exceptions.ROSInterruptException:
             pass
 
+    def get_detection_data(self, req):
+        result = self.results[0]
+
+        box_data = {}
+
+        response = DetectionDataResponse()
+
+        for box in result.boxes:
+            coordinates = box.xyxy[0].tolist()
+            bb_x1, bb_y1, bb_x2, bb_y2 = [round(x) for x in coordinates]
+            bb_cx = (bb_x2 + bb_x1)/2
+            bb_cy = (bb_y1 + bb_y2)/2
+            bb_width = bb_x2 - bb_x1
+            bb_height = bb_y2 - bb_y1
+            class_id = box.cls[0].item()
+            box_data[result.names[class_id]] = [bb_cx, bb_cy, bb_width, bb_height]
+
+        if req.label in box_data.keys():
+            response.bb_cx = box_data[req.label][0]
+            response.bb_cy = box_data[req.label][1]
+            response.bb_width = box_data[req.label][2]
+            response.bb_height = box_data[req.label][3]
+            response.img_width = self.image_res[1]
+            response.img_height = self.image_res[0]
+            response.flag = 'found'
+        else:
+            response.bb_cx = self.image_res[1] / 2
+            response.bb_cy = self.image_res[0] / 2
+            response.bb_width = 0
+            response.bb_height = 0
+            response.img_width = self.image_res[1]
+            response.img_height = self.image_res[0]
+            response.flag = 'not_found'
+
+        return response
 
 if __name__ == "__main__":
     rospy.init_node("image_processor", anonymous=True)
@@ -75,5 +118,3 @@ if __name__ == "__main__":
     image_processor = ImageProcessor()
 
     rospy.spin()
-
-
